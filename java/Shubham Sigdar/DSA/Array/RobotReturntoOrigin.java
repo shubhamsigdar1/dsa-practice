6509918ed9e197bce74e94630d93516c32bb7417
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-12-06 20:12:02 +0530

    Shubhamsigdar done questions of array and string and javascript

--- java/Shubham Sigdar/DSA/Array/RobotReturntoOrigin.java
+++ java/Shubham Sigdar/DSA/Array/RobotReturntoOrigin.java
@@ -16,7 +16,7 @@ package DSA.Array;
 //        Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
 //        Example 2:
 //
-//        Input: moves = "LL
+//        Input: moves = "Ll
 //        Output: false
 //        Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin. We return false because it is not at the origin at the end of its moves.
 public class RobotReturntoOrigin {
